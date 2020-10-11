334e2e6b14eadb2d9f1177eafc0c7598928bea02
AlexeyAnufriev <contact@alexey-anufriev.com>
2020-10-11 23:25:48 +0200

    docs: update description of io producer/consumer

--- src/main/java/ice/bricks/io/IoUnsafeConsumer.java
+++ src/main/java/ice/bricks/io/IoUnsafeConsumer.java
@@ -3,7 +3,7 @@ package ice.bricks.io;
 import java.io.IOException;
 
 /**
- * Represents IO operation that consumes a value may throw checked {@link IOException}.
+ * Represents IO operation that consumes a value and may throw checked {@link IOException}.
  */
 @FunctionalInterface
 public interface IoUnsafeConsumer<T> {
--- src/main/java/ice/bricks/io/IoUnsafeProducer.java
+++ src/main/java/ice/bricks/io/IoUnsafeProducer.java
@@ -3,7 +3,7 @@ package ice.bricks.io;
 import java.io.IOException;
 
 /**
- * Represents IO operation that returns a value may throw checked {@link IOException}.
+ * Represents IO operation that returns a value and may throw checked {@link IOException}.
  */
 @FunctionalInterface
 public interface IoUnsafeProducer<T> {
